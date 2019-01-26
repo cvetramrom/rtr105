c8beae519779b307b30669c73ea4edc742139767
cvetramrom <43995141+cvetramrom@users.noreply.github.com>
2019-01-26 14:57:17 +0200

    Add files via upload

---
+++ dgr_2019_26.py
@@ -0,0 +1,49 @@
+# Fails:181.py
+# Autors: ......
+# Aplieciba numurs: ....
+# Datums: ....
+# Plotting legends in Matplotlib
+
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import matplotlib.collections as mcol
+from matplotlib.legend_handler import HandlerLineCollection, HandlerTuple
+from matplotlib.lines import Line2D
+import numpy as np
+
+t1 = np.arange(0.0, 2.0, 0.1)
+t2 = np.arange(0.0, 2.0, 0.01)
+
+fig, ax = plt.subplots()
+
+# note that plot returns a list of lines.  The "l1, = plot" usage
+# extracts the first element of the list into l1 using tuple
+# unpacking.  So l1 is a Line2D instance, not a sequence of lines
+l1, = ax.plot(t2, np.exp(-t2))
+l2, l3 = ax.plot(t2, np.sin(2 * np.pi * t2), '--o', t1, np.log(1 + t1), '.')
+l4, = ax.plot(t2, np.exp(-t2) * np.sin(2 * np.pi * t2), 's-.')
+
+ax.legend((l2, l4), ('oscillatory', 'damped'), loc='upper right', shadow=True)
+ax.set_xlabel('time')
+ax.set_ylabel('volts')
+ax.set_title('Damped oscillation')
+plt.show()
+x = np.linspace(0, 1)
+
+fig, (ax0, ax1) = plt.subplots(2, 1)
+
+# Plot the lines y=x**n for n=1..4.
+for n in range(1, 5):
+    ax0.plot(x, x**n, label="n={0}".format(n))
+leg = ax0.legend(loc="upper left", bbox_to_anchor=[0, 1],
+                 ncol=2, shadow=True, title="Legend", fancybox=True)
+leg.get_title().set_color("red")
+
+# Demonstrate some more complex labels.
+ax1.plot(x, x**2, label="multi\nline")
+half_pi = np.linspace(0, np.pi / 2)
+ax1.plot(np.sin(half_pi), np.cos(half_pi), label=r"$\frac{1}{2}\pi$")
+ax1.plot(x, 2**(x**2), label="$2^{x^2}$")
+ax1.legend(shadow=True, fancybox=True)
+
+plt.show()
